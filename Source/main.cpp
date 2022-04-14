7f2ea9f38e44b8c3f8627f7316fd0f481600855c
Gero Bodemann <78881613+survivalizeed@users.noreply.github.com>
2022-04-14 13:10:08 +0200

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,9 +2,8 @@
 #include "Trainer.h"
 
 int main() {
-	Trainer a("ac_client.exe");
-	a.AddEntry("ammo", 0x10f4f4, { 0x374, 0x14, 0x0 });
-	a.WriteDynAddress(0x00475E8A, {0x0, 0x1, 0x2, 0x3});
-	std::cout << a.ReadDynAddress<long>(0x00475E8A);
-	a.Write("ammo", long long(123));
+	Trainer a("xyz.exe");	// Connect the Trainer to a process. The process must be running
+	a.AddEntry("ammo", 0x10f4f4, { 0x374, 0x14, 0x0 }); // Add an entry you can refer to with "ammo". Add the static address with its corresponding offsets.
+	a.Write("ammo", 1000)	// Set the ammo to 1000
+	return a.Read<unsigned int>("ammo"); // This should return 1000
 }
