a80a46682162d1eb851c984888c8ee2f72b2f843
Gero Bodemann <78881613+survivalizeed@users.noreply.github.com>
2022-05-15 16:00:39 +0200

    Update example.cpp

--- Source/example.cpp
+++ Source/example.cpp
@@ -2,7 +2,7 @@
 #include "Trainer.h"
 
 int main() {
-	Trainer a("xyz.exe");	// Connect the Trainer to a process. The process must be running
+	Trainer a("xyz.exe");	// Attach the Trainer to a process. The process must be running
 	a.AddEntry("ammo", 0x10f4f4, { 0x374, 0x14, 0x0 }); // Add an entry you can refer to with "ammo". Add the static address with its corresponding offsets.
 	a.Write("ammo", int(1000))	// Set the ammo to 1000
 	return a.Read<int>("ammo"); // This should return 1000
